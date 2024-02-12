2269c719ad6d914466811f7e4d195de654c4a89b
cuiyq <cyq1102051001@gmail.com>
2024-02-12 19:44:07 +0800

    完成实体类编写

---
+++ bank-mvc-001/src/main/java/com/cuiyq/domain/Account.java
@@ -0,0 +1,48 @@
+package com.cuiyq.domain;
+
+/**
+ * @author Cuiyq
+ * @version 1.0
+ * describe：
+ */
+public class Account {
+    private int id;
+    private String actno;
+    private Double balance;
+
+    public Account() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getActno() {
+        return actno;
+    }
+
+    public void setActno(String actno) {
+        this.actno = actno;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Double balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "id=" + id +
+                ", actno='" + actno + '\'' +
+                ", balance=" + balance +
+                '}';
+    }
+}
