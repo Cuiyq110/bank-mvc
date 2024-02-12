de40dd44f5dd23362a59958b24f227e89a4a5679
cuiyq <cyq1102051001@gmail.com>
2024-02-12 19:39:48 +0800

    完成数据库数据表设计

---
+++ sql/console.sql
@@ -0,0 +1,14 @@
+create database mvc;
+use mvc;
+
+create table accout(
+    id int primary key auto_increment,
+    actno varchar(20) not null default '',
+    balance decimal(10,2) not null default 0
+);
+
+insert into accout values(null,'act001', 50000)
+,(null,'act002', 0);
+
+create user 'mvc'@'%' identified by '123456';
+grant all on mvc.*  to 'mvc'@'%';
